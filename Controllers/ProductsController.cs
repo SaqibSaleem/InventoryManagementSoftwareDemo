02a045c8100365e728cf2c86e139a61a92d2db91
Saqib Saleem <saqibsaleem173@gmail.com>
2025-12-25 23:57:33 +0000

    ProductsExportExcel

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Data;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 
 namespace InventoryManagementSoftwareDemo.Controllers
@@ -48,7 +51,7 @@ namespace InventoryManagementSoftwareDemo.Controllers
 				ViewData["ErrorMessage"] = ex.Message;
 				return View(new List<Products>());
 			}
-			
+
 		}
 		// Add Product
 		[HttpPost]
@@ -56,7 +59,7 @@ namespace InventoryManagementSoftwareDemo.Controllers
 		{
 			try
 			{
-				if (products.ItemPictureFile!= null && products.ItemPictureFile.Length>0)
+				if (products.ItemPictureFile != null && products.ItemPictureFile.Length > 0)
 				{
 					string wwwRootPath = _webHostEnvironment.WebRootPath;
 					string FileName = products.ItemPictureFile.FileName;
@@ -74,7 +77,7 @@ namespace InventoryManagementSoftwareDemo.Controllers
 				}
 				products.CreatedDate = DateTime.Now;
 				products.AddedBy = _userManager.GetUserName(User);
-				var Result = _dbContext.Products.FromSqlRaw("SP_Add_Products @p0,@p1,@p2,@p3,@p4,@p5", parameters: new object[] { products.Name,products.Quantity,products.Price,products.ItemPicture, products.AddedBy, products.CreatedDate }).ToList();
+				var Result = _dbContext.Products.FromSqlRaw("SP_Add_Products @p0,@p1,@p2,@p3,@p4,@p5", parameters: new object[] { products.Name, products.Quantity, products.Price, products.ItemPicture, products.AddedBy, products.CreatedDate }).ToList();
 				return RedirectToAction("Index");
 			}
 			catch (Exception ex)
@@ -91,7 +94,7 @@ namespace InventoryManagementSoftwareDemo.Controllers
 			try
 			{
 				var product = _dbContext.Products.Where(x => x.Id == id).FirstOrDefault();
-				if (product!=null)
+				if (product != null)
 				{
 					return product;
 				}
@@ -112,7 +115,7 @@ namespace InventoryManagementSoftwareDemo.Controllers
 			try
 			{
 				var Result = _dbContext.Products.Where(x => x.Id == id).FirstOrDefault();
-				if (Result!=null)
+				if (Result != null)
 				{
 					_dbContext.Products.Remove(Result);
 					_dbContext.SaveChanges();
@@ -149,8 +152,8 @@ namespace InventoryManagementSoftwareDemo.Controllers
 					{
 						products.ItemPicture = productData.ItemPicture;
 					}
-						productData.UpdatedBy = _userManager.GetUserName(User);
-					var Result = _dbContext.Products.FromSqlRaw("SP_Update_Products @p0,@p1,@p2,@p3,@p4,@p5", parameters: new object[] {products.Id, products.Name, products.Quantity, products.Price, products.ItemPicture, productData.UpdatedBy }).ToList();
+					productData.UpdatedBy = _userManager.GetUserName(User);
+					var Result = _dbContext.Products.FromSqlRaw("SP_Update_Products @p0,@p1,@p2,@p3,@p4,@p5", parameters: new object[] { products.Id, products.Name, products.Quantity, products.Price, products.ItemPicture, productData.UpdatedBy }).ToList();
 
 					return RedirectToAction("Index");
 				}
@@ -165,6 +168,82 @@ namespace InventoryManagementSoftwareDemo.Controllers
 			}
 		}
 
+		// Export Products to Excel
+		[HttpPost]
+		public async Task<IActionResult> ExportProductsToExcel()
+		{
+			try
+			{
+				// Get Products List from database
+				var productsList = _dbContext.ProductList.FromSqlRaw("SP_GET_Products").ToList();
+				if (productsList != null && productsList.Count > 0)
+				{
+					using (var workbook = new XLWorkbook())
+					{
+						// Create a new Excel workbook
+						var workSheet = workbook.Worksheets.Add("Products");
 
+						// Create HeaderRow
+						var headerRow = workSheet.Row(1);
+						headerRow.Style.Font.Bold = true;
+						headerRow.Style.Fill.BackgroundColor = XLColor.LightGray;
+
+						// Add Header Columns
+						workSheet.Cell(1, 1).Value = "Sr No.";
+						//workSheet.Cell(1, 2).Value = "Picture";
+						workSheet.Cell(1, 2).Value = "Name";
+						workSheet.Cell(1, 3).Value = "Quantity";
+						workSheet.Cell(1, 4).Value = "Price";
+						workSheet.Cell(1, 5).Value = "Added By";
+
+						// Fill Rows with Data
+						int rowNumber = 2;
+						int serialNumber = 1;
+						foreach (var product in productsList)
+						{
+							workSheet.Cell(rowNumber, 1).Value = serialNumber;
+							//workSheet.Cell(rowNumber, 2).Value = product.ItemPicture;
+							workSheet.Cell(rowNumber, 2).Value = product.Name;
+							workSheet.Cell(rowNumber, 3).Value = product.Quantity;
+							workSheet.Cell(rowNumber, 4).Value = product.Price;
+							workSheet.Cell(rowNumber, 5).Value = product.AddedBy;
+							rowNumber++;
+							serialNumber++;
+						}
+
+						// Sizing the Columns
+						workSheet.Columns().AdjustToContents();
+						// Add border to cells
+						var range = workSheet.Range(1, 1, rowNumber - 1, 5);
+						range.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+						range.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+
+						// Set FileName
+						var fileName = $"Products_{DateTime.Now.ToString("yyyyMMddHHmmss")}.xlsx";
+						var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+						// Save the workbook to a MemoryStream
+						using (var steam = new MemoryStream())
+						{
+							workbook.SaveAs(steam);
+							var content = steam.ToArray();
+							return File(content, contentType, fileName);
+						}
+					}
+
+				}
+				else
+				{
+					TempData["ErrorMessage"] = "Products not found.";
+					return RedirectToAction("Index");
+				}
+			}
+			catch (Exception ex)
+			{
+				TempData["ErrorMessage"] = "Error exporting products. Please try again.";
+				return RedirectToAction("Index");
+			}
+
+		}
 	}
 }
